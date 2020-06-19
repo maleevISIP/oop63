6bde2f32b89b03b3ce0eaf88a7a78469616d7e0d
Egor Maleev <egor.maleev@list.ru>
2020-06-19 21:04:25 +0500

    egorka

---
+++ oop63/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop63
+{
+    class treugole
+    {
+        private bool treugol;
+        private double Alph;
+        private double Bet;
+        private double Gamm;
+
+        public treugole(int l1, int l2, int l3)
+        {
+            double Alpha = (Math.Acos((Math.Pow(l1, 2) + Math.Pow(l2, 2) - Math.Pow(l3, 2)) / (2 * l1 * l2))) * (180 / Math.PI);
+            double Beta = (Math.Acos((Math.Pow(l2, 2) + Math.Pow(l3, 2) - Math.Pow(l1, 2)) / (2 * l2 * l3))) * (180 / Math.PI);
+            double Gamma = (Math.Acos((Math.Pow(l3, 2) + Math.Pow(l1, 2) - Math.Pow(l2, 2)) / (2 * l1 * l3))) * (180 / Math.PI);
+
+            if ((l1 + l2 > l3) && (l2 + l3 > l1) && (l1 + l3 > l2))
+            {
+                treugol = true;
+
+                Alph = Alpha;
+                Bet = Beta;
+                Gamm = Gamma;
+            }
+            else
+            {
+                treugol = false;
+            }
+
+        }
+
+        public void Display()
+        {
+            if (treugol == true)
+            {
+                Console.WriteLine($"\nТреугольник можно построить\n\n" +
+                                  $"Углы\n" +
+                                  $"Alpha : {Alph.ToString("##.##")}" +
+                                  $"\nBeta : {Bet.ToString("##.##")}" +
+                                  $"\nGamma: {Gamm.ToString("##.##")}");
+            }
+            else
+                Console.WriteLine("\nТреугольник нельзя построить");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            treugole trget = new treugole(7, 7, 7);
+            trget.Display();
+            treugole trget1 = new treugole(6, 8, 4);
+            trget1.Display();
+            treugole trget2 = new treugole(32, 4, 13);
+            trget2.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ oop63/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("oop63")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("oop63")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("f9598ca6-31d5-4bc4-bebb-d1a08becea18")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
